098fcc1da81d2d5db2e275135e45df85bbe703ff
JanMarco <marcovarela1290@gmail.com>
2022-09-18 17:25:29 -0500

    Laboratorios

---
+++ Practica02/Saludador.java
@@ -0,0 +1,12 @@
+// Inicio de practica: 10:30pm, 10/Septiembre
+// Definir clase ⬇
+public class Saludador {
+	// ⬇ Metodo para "Saludo" ⬇
+    // Llave inicial ⬇
+	public void saludar() {
+		System.out.println("Hola, Alumno de Programacion Orientada a Objetos 1");
+	} 
+    // ⬆ Llave que cierre el metodo
+}
+// ⬆ Llave final
+// Fin de practica: 11:53pm, 10/Septiembre
---
+++ Practica02/TestSaludador.java
@@ -0,0 +1,15 @@
+//Inicio de practica 11:14pm, 9/Septiembre
+// Definir clase ⬇
+public class TestSaludador {
+	// ⬇ Metodo "main" ⬇
+    // Llave inicial ⬇
+	public static void main(String [] args) {
+		// Se crea una instancia de Saludador.java ⬇
+		Saludador alumno = new Saludador();
+		// Ingresar metodo saludar de alumno. ⬇
+		alumno.saludar();
+	}
+    // ⬆ Llave que cierre el metodo
+}
+// ⬆ Llave final
+// Fin de practica: 12:10pm, 10/Septiembre
