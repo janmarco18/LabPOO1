a09261c4b2e9dcdf29eef6f6e9144f4988d507df
JanMarco <marcovarela1290@gmail.com>
2022-09-30 10:13:17 -0500

    Parcial 1 10:13am

--- Parcial 1/www/soft/parc1/mx/TestSeries.java
+++ Parcial 1/www/soft/parc1/mx/TestSeries.java
@@ -3,8 +3,8 @@ package www.soft.parc1.mx;
 public class TestSeries {
     public static void main(String[]args){
         Serie s = new Serie();
-        int m = s.genNumero(100);
-        int n = s.genNumero(100);
+        int m = s.genNumero(10);
+        int n = s.genNumero(10);
         s.fibo(int)(Math.random()*m*10);
         s.fibo(int)(Math.random()*n*10);
         System.out.println(n);
