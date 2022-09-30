c1768766cb81b78ece92179a0f9ece33839e3456
JanMarco <marcovarela1290@gmail.com>
2022-09-30 08:49:09 -0500

    Parcial 1 08:49am

--- Parcial 1/TestSeries.java
+++ Parcial 1/TestSeries.java
@@ -1,10 +1,12 @@
 // Hora de inicio: 08:05am
 public class TestSeries {
     public static void main(String[]args){
+        Package
         Serie s = new Serie();
         int m = s.genNumero(100);
         int n = s.genNumero(100);
-        s.fibo(int)(Math.random()*x*10);
+        s.fibo(int)(Math.random()*m*10);
+        s.fibo(int)(Math.random()*n*10);
         System.out.println(n);
     }
 }
