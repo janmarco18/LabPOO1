0c442b159fda2bfd5e09c7f28c64d0e6f75bd97f
JanMarco <marcovarela1290@gmail.com>
2022-10-14 07:51:28 -0500

    14 de Octubre 07:50am

--- Practica09/Casas/casa.java
+++ Practica09/Casas/casa.java
@@ -1,1 +1,7 @@
-package www.desasof.mx
+package www.desasof.mx;
+public class casa{
+	public static void main(String[] args) {
+		private String tipo;
+		private int hab;
+		private float costo;
+} 
