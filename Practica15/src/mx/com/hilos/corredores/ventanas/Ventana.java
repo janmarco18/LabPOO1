dd32cc4953267a2087936860a04ecdf442ad8e7f
JanMarco <marcovarela1290@gmail.com>
2022-11-25 08:10:58 -0600

    08:10am

---
+++ Practica15/src/mx/com/hilos/corredores/ventanas/Ventana.java
@@ -0,0 +1,9 @@
+// Inicio de practica 8:00am
+package mx.com.hilos.corredores.ventanas;
+public class Ventana extends JFrame{
+	public static void main(String[] args) {
+		JLabel lblTitulo, lblElem1, lblElem2, hElem1, hElem2;
+		JTextField txtElem1, txtElem2;
+		JButton btnStart;
+	}
+}
