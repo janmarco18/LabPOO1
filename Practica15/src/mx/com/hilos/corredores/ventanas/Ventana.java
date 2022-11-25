9cebdcec634babf910f0a31a4d4a22aa6045d8b7
JanMarco <marcovarela1290@gmail.com>
2022-11-25 08:25:15 -0600

    08:25am

--- Practica15/src/mx/com/hilos/corredores/ventanas/Ventana.java
+++ Practica15/src/mx/com/hilos/corredores/ventanas/Ventana.java
@@ -2,8 +2,29 @@
 package mx.com.hilos.corredores.ventanas;
 public class Ventana extends JFrame{
 	public static void main(String[] args) {
-		JLabel lblTitulo, lblElem1, lblElem2, hElem1, hElem2;
-		JTextField txtElem1, txtElem2;
-		JButton btnStart;
+		private JLabel lblTitulo, lblElem1, lblElem2, hElem1, hElem2;
+		private JTextField txtElem1, txtElem2;
+		private JButton btnStart;
+
+		public Ventana();
+		initComponent();
+			int ancho = 550;
+			int alto = 250;
+			Dimension lblDim = new Dimension(ancho-15,alto/10);
+			Color border = new Color(0, 0, 255);
+				lblTitulo = new JLabel("* Practica 15 Manejo de hilos *");
+				lblTitulo.setPreferredSize(lblDim);
+				lblElem1 = new JLabel("Hilo 1");
+				txtElem1 = new JTextField(3);
+				lblElem2 = new JLabel("Hilo 2");
+				txtElem2 = new JTextField(3);
+				btnStart = new JButton("Comenzar");
+				hElem1 = new JLabel(" Aqui va el hilo 1 ");
+				hElem1.setBorder(BorderFactory.createLineBorder(border));
+				hElem1.setPreferredSize(lblDim);
+				hElem2 = new JLabel(" Aqui va el hilo 2 ");
+				hElem2.setBorder(BorderFactory.createLineBorder(border));
+				hElem2.setPreferredSize(lblDim);
+				btnStart.setPreferredSize(lblDim);
 	}
 }
